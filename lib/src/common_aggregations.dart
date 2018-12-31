423ced58c11d6fb474c29ab556ea7a46b89bd8fe
thumbert <tony.humberts@gmail.com>
2018-12-31 09:27:51 -0500

    Update common_aggregations.dart

--- lib/src/common_aggregations.dart
+++ lib/src/common_aggregations.dart
@@ -1,6 +1,7 @@
 library common_aggregations;
 
 import 'timeseries_base.dart';
+import 'package:timezone/timezone.dart';
 import 'package:timeseries/timeseries.dart';
 import 'package:date/date.dart';
 
@@ -14,7 +15,7 @@ TimeSeries<T> toHourly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
   var grp = <Interval, List<K>>{};
   int N = x.length;
   for (int i = 0; i < N; i++) {
-    var date = Hour.fromTZDateTime(x[i].interval.start);
+    var date = Hour.containing(x[i].interval.start);
     grp.putIfAbsent(date, () => <K>[]).add(x[i].value);
   }
   return new TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
@@ -59,7 +60,8 @@ TimeSeries<T> toYearly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
   int N = x.length;
   for (int i = 0; i < N; i++) {
     var start = x[i].interval.start;
-    var year = Interval(start.year, start.year+1);
+    var year = Interval(TZDateTime(start.location, start.year), 
+                        TZDateTime(start.location, start.year+1));
     grp.putIfAbsent(month, () => <K>[]).add(x[i].value);
   }
   return new TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
