e201f9ec705a478c50124f2d8563196cfaddae7e
thumberts <tony.humberts@gmail.com>
2020-03-26 09:38:31 -0400

    add toWeekly

--- lib/src/common_aggregations.dart
+++ lib/src/common_aggregations.dart
@@ -5,7 +5,6 @@ import 'package:timezone/timezone.dart';
 import 'package:timeseries/timeseries.dart';
 import 'package:date/date.dart';
 
-
 /// Convenience function to calculate an hourly summary.  Function f takes an
 /// Iterable of values and returns the summary statistic.  The TimeSeries [x]
 /// should not cross day boundaries.
@@ -13,12 +12,12 @@ import 'package:date/date.dart';
 /// Implementation is more efficient than the simple groupByIndex + map.
 TimeSeries<T> toHourly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
   var grp = <Interval, List<K>>{};
-  int N = x.length;
-  for (int i = 0; i < N; i++) {
+  var N = x.length;
+  for (var i = 0; i < N; i++) {
     var date = Hour.containing(x[i].interval.start);
     grp.putIfAbsent(date, () => <K>[]).add(x[i].value);
   }
-  return new TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
+  return TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
 }
 
 
@@ -29,12 +28,26 @@ TimeSeries<T> toHourly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
 /// Implementation is more efficient than the simple groupByIndex + map.
 TimeSeries<T> toDaily<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
   var grp = <Interval, List<K>>{};
-  int N = x.length;
-  for (int i = 0; i < N; i++) {
+  var N = x.length;
+  for (var i = 0; i < N; i++) {
     var date = Date.fromTZDateTime(x[i].interval.start);
     grp.putIfAbsent(date, () => <K>[]).add(x[i].value);
   }
-  return new TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
+  return TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
+}
+
+
+/// Convenience function to calculate a weekly summary.  Function f takes an
+/// Iterable of values and returns the summary statistic.  The TimeSeries [x]
+/// should not cross week boundaries.
+TimeSeries<T> toWeekly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
+  var grp = <Interval, List<K>>{};
+  var N = x.length;
+  for (var i = 0; i < N; i++) {
+    var week = Week.fromTZDateTime(x[i].interval.start);
+    grp.putIfAbsent(week, () => <K>[]).add(x[i].value);
+  }
+  return TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
 }
 
 
@@ -43,12 +56,12 @@ TimeSeries<T> toDaily<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
 /// should not cross month boundaries.
 TimeSeries<T> toMonthly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
   var grp = <Interval, List<K>>{};
-  int N = x.length;
-  for (int i = 0; i < N; i++) {
+  var N = x.length;
+  for (var i = 0; i < N; i++) {
     var month = Month.fromTZDateTime(x[i].interval.start);
     grp.putIfAbsent(month, () => <K>[]).add(x[i].value);
   }
-  return new TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
+  return TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
 }
 
 
@@ -57,13 +70,13 @@ TimeSeries<T> toMonthly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
 /// should not cross month boundaries.
 TimeSeries<T> toYearly<K,T>(TimeSeries<K> x, T Function(List<K>) f) {
   var grp = <Interval, List<K>>{};
-  int N = x.length;
-  for (int i = 0; i < N; i++) {
+  var N = x.length;
+  for (var i = 0; i < N; i++) {
     var start = x[i].interval.start;
     var year = Interval(TZDateTime(start.location, start.year), 
                         TZDateTime(start.location, start.year+1));
     grp.putIfAbsent(year, () => <K>[]).add(x[i].value);
   }
-  return new TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
+  return TimeSeries.from(grp.keys, grp.values.map((xs) => f(xs)));
 }
 
